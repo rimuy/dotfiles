f6f03d1a31926cfd3ef23520b95d21371ee9e95a
Kevinwkz <kevinntavares@gmail.com>
2020-07-05 03:50:22 -0300

    changed colorscheme

--- .config/dmenu/config.def.h
+++ .config/dmenu/config.def.h
@@ -5,13 +5,14 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=10",
-    "JoyPixels:pixelsize=12:antialias=true:autohint=true"
+    "Noto Color Emoji:pixelsize=12:antialias=true:autohint=true"
+    //"JoyPixels:pixelsize=12:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#CFCFCF", "#111111" },
-	[SchemeSel] = { "#FEFEFE", "#1565C0" },
+	[SchemeNorm] = { "#fefefe", "#383c4a" },
+	[SchemeSel] = { "#383c4a", "#5294e2" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
